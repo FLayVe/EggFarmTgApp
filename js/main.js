3b6d7c861fdd8edc832fc0daaf576fa247bacd18
Smate <xvava8442@gmail.com>
2024-05-12 00:47:41 +0200

    not-copy

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 
-window.addEventListener(
-	'mousewheel',
+// window.addEventListener(
+// 	'mousewheel',
+// 	function (e) {
+// 		if (e.ctrlKey) {
+// 			e.preventDefault()
+// 			return false
+// 		}
+// 	},
+// 	{ passive: false }
+// )
+
+document.addEventListener(
+	'contextmenu',
 	function (e) {
-		if (e.ctrlKey) {
-			e.preventDefault()
-			return false
-		}
+		e.preventDefault()
 	},
-	{ passive: false }
+	false
 )

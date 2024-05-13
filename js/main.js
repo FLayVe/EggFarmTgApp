a2307eb9cf57cbb4f5792859f061067566bd05b3
Smate <xvava8442@gmail.com>
2024-05-13 03:33:40 +0200

    popup

--- js/main.js
+++ js/main.js
@@ -1,19 +1,100 @@
+document.getElementById('grain').addEventListener('mouseover', function () {
+	this.classList.add('mine__item-hov')
+})
 
-// window.addEventListener(
-// 	'mousewheel',
-// 	function (e) {
-// 		if (e.ctrlKey) {
-// 			e.preventDefault()
-// 			return false
-// 		}
-// 	},
-// 	{ passive: false }
-// )
+document.getElementById('incubators').addEventListener('mouseover', function () {
+		this.classList.add('mine__item-hov')
+})
 
-// document.addEventListener(
-// 	'contextmenu',
-// 	function (e) {
-// 		e.preventDefault()
-// 	},
-// 	false
-// )
+document.getElementById('vitamins').addEventListener('mouseover', function () {
+		this.classList.add('mine__item-hov')
+})
+
+document.getElementById('trainer').addEventListener('mouseover', function () {
+		this.classList.add('mine__item-hov')
+})
+
+document.getElementById('elixir').addEventListener('mouseover', function () {
+		this.classList.add('mine__item-hov')
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+	document
+		.getElementById('popup__btn-grain')
+		.addEventListener('click', function () {
+			document.getElementById('myOverlay').style.display = 'none'
+			document.querySelector('main').classList.remove('grain-open')
+			document.getElementById('grain').classList.remove('mine__item-hov')
+		})
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+	document
+		.getElementById('popup__btn-incubators')
+		.addEventListener('click', function () {
+			document.getElementById('myOverlay').style.display = 'none'
+			document.querySelector('main').classList.remove('incubators-open')
+			document.getElementById('incubators').classList.remove('mine__item-hov')
+		})
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+	document
+		.getElementById('popup__btn-vitamins')
+		.addEventListener('click', function () {
+			document.getElementById('myOverlay').style.display = 'none'
+			document.querySelector('main').classList.remove('vitamins-open')
+			document.getElementById('vitamins').classList.remove('mine__item-hov')
+		})
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+	document
+		.getElementById('popup__btn-trainer')
+		.addEventListener('click', function () {
+			document.getElementById('myOverlay').style.display = 'none'
+			document.querySelector('main').classList.remove('trainer-open')
+			document.getElementById('trainer').classList.remove('mine__item-hov')
+		})
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+	document
+		.getElementById('popup__btn-elixir')
+		.addEventListener('click', function () {
+			document.getElementById('myOverlay').style.display = 'none'
+			document.querySelector('main').classList.remove('elixir-open')
+			document.getElementById('elixir').classList.remove('mine__item-hov')
+		})
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('grain').addEventListener('click', function () {
+		document.getElementById('myOverlay').style.display = 'block'
+		document.querySelector('main').classList.toggle('grain-open')
+	})
+})
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('incubators').addEventListener('click', function () {
+		document.getElementById('myOverlay').style.display = 'block'
+		document.querySelector('main').classList.toggle('incubators-open')
+	})
+})
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('vitamins').addEventListener('click', function () {
+		document.getElementById('myOverlay').style.display = 'block'
+		document.querySelector('main').classList.toggle('vitamins-open')
+	})
+})
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('trainer').addEventListener('click', function () {
+		document.getElementById('myOverlay').style.display = 'block'
+		document.querySelector('main').classList.toggle('trainer-open')
+	})
+})
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('elixir').addEventListener('click', function () {
+		document.getElementById('myOverlay').style.display = 'block'
+		document.querySelector('main').classList.toggle('elixir-open')
+	})
+})

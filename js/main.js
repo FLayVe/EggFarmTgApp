7674f79f011d023face44487bf675bb43b1f7017
Smate <xvava8442@gmail.com>
2024-05-22 01:32:41 +0200

    js optm

--- js/main.js
+++ js/main.js
@@ -8,67 +8,8 @@ function toggleHover(element) {
 	})
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-	const sections = document.querySelectorAll('.page')
-	const buttons = document.querySelectorAll('.menu__btn')
-	const content = document.querySelectorAll('.menu__list-text')
-
-	buttons.forEach(button => {
-		button.addEventListener('click', () => {
-			// Remove 'active' class from all sections, buttons, and paths
-			sections.forEach(section => section.classList.remove('active'))
-			buttons.forEach(btn => btn.classList.remove('active'))
-			content.forEach(div => div.classList.remove('active'))
-			document
-				.querySelectorAll('.path')
-				.forEach(path => path.classList.remove('active-path'))
-			document
-				.querySelectorAll('.path')
-				.forEach(path => path.classList.remove('home-path'))
-			document
-				.querySelectorAll('.path-2')
-				.forEach(path => path.classList.remove('active-path'))
-			document
-				.querySelectorAll('.path-2')
-				.forEach(path => path.classList.remove('home-path-2'))
-
-			// Add 'active' class based on button clicked
-			switch (true) {
-				case button.classList.contains('home-btn'):
-					document.getElementById('home').classList.add('active')
-					document.getElementById('home-text').classList.add('active')
-					break
-				case button.classList.contains('mine-btn'):
-					document.getElementById('mine').classList.add('active')
-					document.getElementById('mine-text').classList.add('active')
-					break
-				case button.classList.contains('friends-btn'):
-					document.getElementById('friends').classList.add('active')
-					document.getElementById('friends-text').classList.add('active')
-					break
-				case button.classList.contains('tasks-btn'):
-					document.getElementById('tasks').classList.add('active')
-					document.getElementById('tasks-text').classList.add('active')
-					break
-			}
-
-			// Add 'active' class to the clicked button and its paths
-			button.classList.add('active')
-			button.querySelector('.path').classList.add('active-path')
-			button.querySelector('.path-2').classList.add('active-path')
-		})
-	})
-
-	// Set initial active states for 'home' section
-	document.getElementById('home').classList.add('active')
-	document.querySelector('.home-text').classList.add('active')
-	document.querySelector('.home-path').classList.add('active-path')
-	document.querySelector('.home-path-2').classList.add('active-path')
-})
-
-
 function setupElementClick(elementId, overlayId, mainClass, itemHoverClass) {
-	var element = document.getElementById(elementId)
+	const element = document.getElementById(elementId)
 	element.addEventListener('click', function () {
 		document.getElementById(overlayId).style.display = 'block'
 		document.querySelector('main').classList.add(mainClass)
@@ -77,7 +18,7 @@ function setupElementClick(elementId, overlayId, mainClass, itemHoverClass) {
 }
 
 function setupPopupButton(btnId, overlayId, mainClass, targetElements) {
-	var btn = document.getElementById(btnId)
+	const btn = document.getElementById(btnId)
 	btn.addEventListener('click', function () {
 		document.getElementById(overlayId).style.display = 'none'
 		document.querySelector('main').classList.remove(mainClass)
@@ -90,7 +31,53 @@ function setupPopupButton(btnId, overlayId, mainClass, targetElements) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-	var elements = [
+	const sections = document.querySelectorAll('.page')
+	const buttons = document.querySelectorAll('.menu__btn')
+	const content = document.querySelectorAll('.menu__list-text')
+	const paths = document.querySelectorAll('.path')
+	const paths2 = document.querySelectorAll('.path-2')
+
+	function handleButtonClick(button) {
+		sections.forEach(section => section.classList.remove('active'))
+		buttons.forEach(btn => btn.classList.remove('active'))
+		content.forEach(div => div.classList.remove('active'))
+		paths.forEach(path => {
+			path.classList.remove('active-path')
+			path.classList.remove('home-path')
+		})
+		paths2.forEach(path => {
+			path.classList.remove('active-path')
+			path.classList.remove('home-path-2')
+		})
+
+		const buttonMapping = {
+			'home-btn': 'home',
+			'mine-btn': 'mine',
+			'friends-btn': 'friends',
+			'tasks-btn': 'tasks',
+		}
+
+		for (const [btnClass, sectionId] of Object.entries(buttonMapping)) {
+			if (button.classList.contains(btnClass)) {
+				document.getElementById(sectionId).classList.add('active')
+				document.getElementById(`${sectionId}-text`).classList.add('active')
+				break
+			}
+		}
+
+		button.classList.add('active')
+		button.querySelector('.path').classList.add('active-path')
+		button.querySelector('.path-2').classList.add('active-path')
+	}
+
+	buttons.forEach(button => {
+		button.addEventListener('click', () => handleButtonClick(button))
+	})
+
+	// Set initial active states for 'home' section
+	handleButtonClick(document.querySelector('.home-btn'))
+
+	const elements = [
 		'grain',
 		'incubators',
 		'vitamins',
@@ -99,7 +86,8 @@ document.addEventListener('DOMContentLoaded', function () {
 		'level',
 	]
 	elements.forEach(function (elementId) {
-		toggleHover(document.getElementById(elementId))
+		const element = document.getElementById(elementId)
+		toggleHover(element)
 		setupElementClick(
 			elementId,
 			'myOverlay',

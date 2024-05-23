fe37d62048d3c3deb3a255637a5f60c661199ab1
Smate <xvava8442@gmail.com>
2024-05-23 18:35:02 +0200

    tap time

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ document.querySelector('.tap__chick').addEventListener('click', function () {
 	// Після завершення анімації видаляємо клас, щоб можна було анімувати знову
 	setTimeout(() => {
 		this.classList.remove('animate')
-	}, 250) // Час повинен співпадати з часом у transition
+	}, 100) // Час повинен співпадати з часом у transition
 })
 
 function toggleHover(element) {

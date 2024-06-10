6fe0482837faed4710a1ea7f75216fe15159e896
Smate <xvava8442@gmail.com>
2024-06-10 04:56:18 +0300

    js opt

--- js/main.js
+++ js/main.js
@@ -5,20 +5,16 @@ document.addEventListener('DOMContentLoaded', function () {
 	let name
 
 	function updateUserText(selector, text) {
-		const elements = document.querySelectorAll(selector)
-		elements.forEach(element => {
+		document.querySelectorAll(selector).forEach(element => {
 			element.textContent = text
 		})
 	}
 
 	if (userData) {
-		if (userData.username && userData.username.startsWith('@')) {
-			name = userData.username
-		} else if (userData.first_name) {
-			name = userData.first_name
-		} else {
-			name = 'User'
-		}
+		name =
+			userData.username && userData.username.startsWith('@')
+				? userData.username
+				: userData.first_name || 'User'
 	} else {
 		name = 'User'
 	}
@@ -39,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 			animateBalanceChange(balance, newBalance)
 			localStorage.setItem('balance', newBalance)
-			balance = newBalance // Update the global balance variable
+			balance = newBalance
 
 			Array.from(event.touches).forEach(touch => {
 				const numberElement = document.createElement('div')
@@ -101,15 +97,9 @@ document.addEventListener('DOMContentLoaded', function () {
 		qrCodeContainer.style.display = 'none'
 		contentContainer.style.display = 'block'
 	}
-})
 
-document.addEventListener('DOMContentLoaded', function () {
 	const timerElement = document.getElementById('timer')
-
-	// Задаємо початковий час у секундах: 28 днів
 	const initialTimeInSeconds = 28 * 24 * 60 * 60
-
-	// Отримуємо збережений час з localStorage або використовуємо початковий час
 	let timeRemaining =
 		localStorage.getItem('timeRemaining') !== null
 			? parseInt(localStorage.getItem('timeRemaining'), 10)
@@ -133,221 +123,201 @@ document.addEventListener('DOMContentLoaded', function () {
 		} else {
 			timeRemaining = initialTimeInSeconds // Перезапуск таймера
 		}
-		// Зберігаємо залишок часу в localStorage
-		localStorage.setItem('timeRemaining', timeRemaining)
+		localStorage.setItem('timeRemaining', timeRemaining) // Зберігаємо залишок часу в localStorage
 	}
 
 	setInterval(updateTimer, 1000) // Оновлення таймера кожну секунду
+	updateTimer() // Ініціалізуємо таймер відразу
 
-	// Ініціалізуємо таймер відразу
-	updateTimer()
-	
-})
+	window.onload = function () {
+		const overlay = document.getElementById('myOverlay')
+		const mainElement = document.querySelector('main')
+		const preloader = document.getElementById('preloader')
+		const menu = document.getElementById('menu')
+		const popupBtn = document.getElementById('popup__btn')
+		const sections = document.querySelectorAll('.page')
+		const buttons = document.querySelectorAll('.menu__btn')
+		const content = document.querySelectorAll('.menu__list-text')
+		const paths = document.querySelectorAll('.path')
+		const paths2 = document.querySelectorAll('.path-2')
+		const tapChick = document.querySelector('.tap__chick')
+		const headerInnerLinks = document.querySelectorAll('.header__inner')
+		const boostLinks = document.querySelectorAll('.get__boost, .get__energy')
 
+		const elements = [
+			'grain',
+			'incubators',
+			'vitamins',
+			'trainer',
+			'elixir',
+			'level',
+		]
+			.map(id => document.getElementById(id))
+			.filter(el => el !== null)
 
-window.onload = function () {
-	// Cached DOM elements
-	const overlay = document.getElementById('myOverlay')
-	const mainElement = document.querySelector('main')
-	const preloader = document.getElementById('preloader')
-	const menu = document.getElementById('menu')
-	const popupBtn = document.getElementById('popup__btn')
-	const sections = document.querySelectorAll('.page')
-	const buttons = document.querySelectorAll('.menu__btn')
-	const content = document.querySelectorAll('.menu__list-text')
-	const paths = document.querySelectorAll('.path')
-	const paths2 = document.querySelectorAll('.path-2')
-	const tapChick = document.querySelector('.tap__chick')
-	const headerInnerLinks = document.querySelectorAll('.header__inner')
-	const boostLinks = document.querySelectorAll('.get__boost, .get__energy')
+		const buttonMapping = {
+			'home-btn': 'home',
+			'mine-btn': 'mine',
+			'friends-btn': 'friends',
+			'tasks-btn': 'tasks',
+		}
 
-	const elements = [
-		'grain',
-		'incubators',
-		'vitamins',
-		'trainer',
-		'elixir',
-		'level',
-	]
-		.map(id => document.getElementById(id))
-		.filter(el => el !== null)
+		if (tapChick) {
+			tapChick.addEventListener('click', function () {
+				this.classList.add('animate')
+				setTimeout(() => {
+					this.classList.remove('animate')
+				}, 30)
+			})
+		}
 
-	const buttonMapping = {
-		'home-btn': 'home',
-		'mine-btn': 'mine',
-		'friends-btn': 'friends',
-		'tasks-btn': 'tasks',
-	}
+		function toggleHover(element) {
+			element.addEventListener('mouseover', function () {
+				this.classList.add('mine__item-hov')
+			})
+			element.addEventListener('mouseout', function () {
+				this.classList.remove('mine__item-hov')
+			})
+		}
 
-	// Function to handle animation on click
-	if (tapChick) {
-		tapChick.addEventListener('click', function () {
-			this.classList.add('animate')
-			setTimeout(() => {
-				this.classList.remove('animate')
-			}, 30)
+		function setupElementClick(element, overlay, mainClass, itemHoverClass) {
+			element.addEventListener('click', function () {
+				overlay.style.display = 'block'
+				mainElement.classList.add(mainClass)
+				element.classList.add(itemHoverClass)
+			})
+		}
+
+		if (popupBtn) {
+			popupBtn.addEventListener('click', function () {
+				overlay.style.display = 'none'
+				mainElement.classList.remove('popup-open')
+				elements.forEach(element => {
+					element.classList.remove('mine__item-hov--block')
+				})
+			})
+		}
+
+		function handleButtonClick(button) {
+			sections.forEach(section => section.classList.remove('active'))
+			buttons.forEach(btn => btn.classList.remove('active'))
+			content.forEach(div => div.classList.remove('active'))
+			paths.forEach(path => {
+				path.classList.remove('active-path')
+				path.classList.remove('home-path')
+			})
+			paths2.forEach(path => {
+				path.classList.remove('active-path')
+				path.classList.remove('home-path-2')
+			})
+
+			for (const [btnClass, sectionId] of Object.entries(buttonMapping)) {
+				if (button.classList.contains(btnClass)) {
+					const section = document.getElementById(sectionId)
+					const textElement = document.getElementById(`${sectionId}-text`)
+					if (section) section.classList.add('active')
+					if (textElement) textElement.classList.add('active')
+					break
+				}
+			}
+
+			button.classList.add('active')
+			const pathElement = button.querySelector('.path')
+			const path2Element = button.querySelector('.path-2')
+			if (pathElement) {
+				pathElement.classList.add('active-path')
+			}
+			if (path2Element) {
+				path2Element.classList.add('active-path')
+			}
+		}
+
+		buttons.forEach(button => {
+			button.addEventListener('click', () => handleButtonClick(button))
 		})
-	}
 
-	// Function to toggle hover class
-	function toggleHover(element) {
-		element.addEventListener('mouseover', function () {
-			this.classList.add('mine__item-hov')
-		})
-		element.addEventListener('mouseout', function () {
-			this.classList.remove('mine__item-hov')
-		})
-	}
+		if (/Mobi|Android/i.test(navigator.userAgent)) {
+			preloader.style.display = 'block'
+			setTimeout(function () {
+				handleButtonClick(document.querySelector('.home-btn'))
+				preloader.style.display = 'none'
+				menu.style.display = 'block'
+			}, 1450)
+		} else {
+			handleButtonClick(document.querySelector('.home-btn'))
+			menu.style.display = 'block'
+		}
 
-	// Function to setup element click
-	function setupElementClick(element, overlay, mainClass, itemHoverClass) {
-		element.addEventListener('click', function () {
-			overlay.style.display = 'block'
-			mainElement.classList.add(mainClass)
-			element.classList.add(itemHoverClass)
+		elements.forEach(element => {
+			toggleHover(element)
+			setupElementClick(element, overlay, 'popup-open', 'mine__item-hov--block')
 		})
-	}
 
-	// Function to setup popup button
-	if (popupBtn) {
-		popupBtn.addEventListener('click', function () {
-			overlay.style.display = 'none'
-			mainElement.classList.remove('popup-open')
-			elements.forEach(element => {
-				element.classList.remove('mine__item-hov--block')
+		function handleLinkClick(targetId) {
+			sections.forEach(section => section.classList.remove('active'))
+			buttons.forEach(button => button.classList.remove('active'))
+			content.forEach(div => div.classList.remove('active'))
+
+			const targetSection = document.getElementById(targetId)
+			if (targetSection) targetSection.classList.add('active')
+
+			paths.forEach(path => {
+				path.classList.remove('active-path')
+				path.classList.remove('home-path')
+			})
+			paths2.forEach(path => {
+				path.classList.remove('active-path')
+				path.classList.remove('home-path-2')
+			})
+		}
+
+		headerInnerLinks.forEach(link => {
+			link.addEventListener('click', function () {
+				handleLinkClick('user')
 			})
 		})
-	}
 
-	// Function to handle menu button click
-	function handleButtonClick(button) {
-		sections.forEach(section => section.classList.remove('active'))
-		buttons.forEach(btn => btn.classList.remove('active'))
-		content.forEach(div => div.classList.remove('active'))
-		paths.forEach(path => {
-			path.classList.remove('active-path')
-			path.classList.remove('home-path')
-		})
-		paths2.forEach(path => {
-			path.classList.remove('active-path')
-			path.classList.remove('home-path-2')
+		boostLinks.forEach(link => {
+			link.addEventListener('click', function () {
+				handleLinkClick('boost')
+			})
 		})
 
-		for (const [btnClass, sectionId] of Object.entries(buttonMapping)) {
-			if (button.classList.contains(btnClass)) {
-				const section = document.getElementById(sectionId)
-				const textElement = document.getElementById(`${sectionId}-text`)
-				if (section) section.classList.add('active')
-				if (textElement) textElement.classList.add('active')
-				break
-			}
+		const copyButton = document.getElementById('friends__btn')
+		const link = document.getElementById('friends__link')
+		const customAlert = document.getElementById('customAlert')
+
+		function copyLinkToClipboard() {
+			const linkHref = link.href
+
+			navigator.clipboard.writeText(linkHref).then(
+				function () {
+					console.log('Link copied')
+					showCustomAlert()
+				},
+				function (err) {
+					console.error('Error: ', err)
+				}
+			)
 		}
 
-		button.classList.add('active')
-		const pathElement = button.querySelector('.path')
-		const path2Element = button.querySelector('.path-2')
-		if (pathElement) {
-			pathElement.classList.add('active-path')
+		function showCustomAlert() {
+			customAlert.classList.remove('hidden')
+			customAlert.classList.add('show')
+			setTimeout(hideCustomAlert, 2000) // Автоматично приховати через 2 секунди
 		}
-		if (path2Element) {
-			path2Element.classList.add('active-path')
+
+		function hideCustomAlert() {
+			customAlert.classList.remove('show')
+			setTimeout(function () {
+				customAlert.classList.add('hidden')
+			}, 500) // Затримка, щоб завершити анімацію
 		}
-	}
 
-	// Adding click event listeners to buttons
-	buttons.forEach(button => {
-		button.addEventListener('click', () => handleButtonClick(button))
-	})
-
-	// Preloader logic
-	if (/Mobi|Android/i.test(navigator.userAgent)) {
-		preloader.style.display = 'block'
-		setTimeout(function () {
-			handleButtonClick(document.querySelector('.home-btn'))
-			preloader.style.display = 'none'
-			menu.style.display = 'block'
-		}, 1450)
-	} else {
-		handleButtonClick(document.querySelector('.home-btn'))
-		menu.style.display = 'block'
-	}
-
-	// Setup hover and click for elements
-	elements.forEach(element => {
-		toggleHover(element)
-		setupElementClick(element, overlay, 'popup-open', 'mine__item-hov--block')
-	})
-
-	// Function to handle link clicks
-	function handleLinkClick(targetId) {
-		sections.forEach(section => section.classList.remove('active'))
-		buttons.forEach(button => button.classList.remove('active'))
-		content.forEach(div => div.classList.remove('active'))
-
-		const targetSection = document.getElementById(targetId)
-		if (targetSection) targetSection.classList.add('active')
-
-		paths.forEach(path => {
-			path.classList.remove('active-path')
-			path.classList.remove('home-path')
-		})
-		paths2.forEach(path => {
-			path.classList.remove('active-path')
-			path.classList.remove('home-path-2')
+		copyButton.addEventListener('click', copyLinkToClipboard)
+		link.addEventListener('click', function (event) {
+			event.preventDefault()
+			copyLinkToClipboard()
 		})
 	}
-
-	// Adding click event listeners to links
-	headerInnerLinks.forEach(link => {
-		link.addEventListener('click', function () {
-			handleLinkClick('user')
-		})
-	})
-
-	boostLinks.forEach(link => {
-		link.addEventListener('click', function () {
-			handleLinkClick('boost')
-		})
-	})
-}
-
-
-document.addEventListener('DOMContentLoaded', function () {
-	
-	const copyButton = document.getElementById('friends__btn')
-	const link = document.getElementById('friends__link')
-	const customAlert = document.getElementById('customAlert')
-
-	function copyLinkToClipboard() {
-		const linkHref = link.href
-
-		navigator.clipboard.writeText(linkHref).then(
-			function () {
-				console.log('Link copied')
-				showCustomAlert()
-			},
-			function (err) {
-				console.error('Error: ', err)
-			}
-		)
-	}
-
-	function showCustomAlert() {
-		customAlert.classList.remove('hidden')
-		customAlert.classList.add('show')
-		setTimeout(hideCustomAlert, 2000) // Автоматично приховати через 2 секунди
-	}
-
-	function hideCustomAlert() {
-		customAlert.classList.remove('show')
-		setTimeout(function () {
-			customAlert.classList.add('hidden')
-		}, 500) // Затримка, щоб завершити анімацію
-	}
-
-	copyButton.addEventListener('click', copyLinkToClipboard)
-	link.addEventListener('click', function (event) {
-		event.preventDefault()
-		copyLinkToClipboard()
-	})
 })
